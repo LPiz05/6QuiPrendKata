a0c247a97eb80bd58d791c16061faacb140db896
Luis <pereiravazluis@gmail.com>
2020-04-09 20:04:53 +0200

    Solution

--- java/SixQuiPrend.java
+++ java/SixQuiPrend.java
@@ -7,7 +7,19 @@ public class SixQuiPrend {
     public static int findNumberOfBullHeads(int value) {
         // TODO
 
-        return 0;
+        //la valeur de base d'une carte est 1
+        //si la carte est multiple de 5, la valeur augmente de 1
+        //si la carte est multiple de 10, la valeur augmente encore de 1
+        //si les chiffres des dizaines et unités sont les memes, la valeur augmente de 4 (sauf pour le numéro 100)
+        //la carte 55 vaut 7 têtes de boeufs
+
+        int bullHeads = 1;
+
+        bullHeads += (value % 5 == 0) ? 1:0;
+        bullHeads += (value % 10 == 0) ? 1:0;
+        bullHeads += ((value % 11 == 0) && (value != 100)) ? 4:0;
+
+        return (value == 55) ? 7:bullHeads;
     }
 
     public static void gameLoop() {
@@ -16,3 +28,4 @@ public class SixQuiPrend {
         }
     }
 }
+
